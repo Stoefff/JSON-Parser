4fb85ef2e82c1228453eabecce40c2cd18d04dea
Yulian Stoev <xxthespinerxx@gmail.com>
2018-06-28 02:13:08 +0300

    Add project files.

---
+++ JSON-Parser/main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+
+	cout << "Hello fucking world :)" << endl;
+
+	//Dank memes melt steal beans 
+
+	return 0;
+}
